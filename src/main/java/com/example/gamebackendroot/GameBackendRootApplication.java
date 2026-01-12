bdf92c555eb97f15dd7335aceef44dcb923ca964
杰拉德 <447121475@bupt.edu.cn>
2026-01-12 10:44:19 +0800

    初始掼蛋

---
+++ src/main/java/com/example/gamebackendroot/GameBackendRootApplication.java
@@ -0,0 +1,13 @@
+package com.example.gamebackendroot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GameBackendRootApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GameBackendRootApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/gamebackendroot/GameBackendRootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.gamebackendroot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GameBackendRootApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
